045b6ab34b99a849c6fb2dd3ebeed8b806752dff
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-16 02:47:54 -0300

    adding new migration "ALTER TABLE"

---
+++ board/src/main/resources/db/changelog/migrations/db.changelog-202503160244.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset marcelo:202503160244
+--comment: set unblock_reason nullable
+
+ALTER TABLE BLOCKS MODIFY COLUMN unblock_reason VARCHAR(255) NULL;
+
+--rollback ALTER TABLE BLOCKS MODIFY COLUMN unblock_reason VARCHAR(255) NOT NULL;
