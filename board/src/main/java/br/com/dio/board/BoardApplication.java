c4c8498e435178775e723b864bd0d4fb7b0640b7
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-16 03:35:38 -0300

    adding MainMenu to the main file

--- board/src/main/java/br/com/dio/board/BoardApplication.java
+++ board/src/main/java/br/com/dio/board/BoardApplication.java
@@ -1,6 +1,7 @@
 package br.com.dio.board;
 
 import br.com.dio.board.persistence.migration.MigrationStrategy;
+import br.com.dio.board.ui.MainMenu;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLException;
@@ -10,12 +11,11 @@ import static br.com.dio.board.persistence.config.ConnectionConfig.getConnection
 @SpringBootApplication
 public class BoardApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		try(var connection = getConnection()){
 			new MigrationStrategy(connection).executeMigration();
-		} catch (SQLException e) {
-            e.printStackTrace();
-        }
+		}
+		new MainMenu().execute();
 	}
 
 }
