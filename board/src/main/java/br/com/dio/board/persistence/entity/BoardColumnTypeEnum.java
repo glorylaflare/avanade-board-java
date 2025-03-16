a3ad872440179e87d65aed545a2e0cc9ad3ff3ca
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-16 01:21:43 -0300

    adding the method findByNome

--- board/src/main/java/br/com/dio/board/persistence/entity/BoardColumnTypeEnum.java
+++ board/src/main/java/br/com/dio/board/persistence/entity/BoardColumnTypeEnum.java
@@ -1,7 +1,13 @@
 package br.com.dio.board.persistence.entity;
 
+import java.util.stream.Stream;
+
 public enum BoardColumnTypeEnum {
 
-    INITIAL, FINAL, CANCEL, PENDING
+    INITIAL, FINAL, CANCEL, PENDING;
+
+    public static BoardColumnTypeEnum findByNome(final String name) {
+        return Stream.of(BoardColumnTypeEnum.values()).filter(b -> b.name().equals(name)).findFirst().orElseThrow();
+    }
 
 }
