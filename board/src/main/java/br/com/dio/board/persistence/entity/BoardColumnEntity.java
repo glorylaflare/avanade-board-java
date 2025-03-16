681b3952e97dd18d0d25e98d89e578fece309f9f
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-15 23:48:05 -0300

    add new variables board and cards

--- board/src/main/java/br/com/dio/board/persistence/entity/BoardColumnEntity.java
+++ board/src/main/java/br/com/dio/board/persistence/entity/BoardColumnEntity.java
@@ -2,11 +2,16 @@ package br.com.dio.board.persistence.entity;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class BoardColumnEntity {
 
     private Long id;
     private String name;
     private Integer order;
-    private BoardColumnTypes tyoes;
+    private BoardColumnTypeEnum type;
+    private BoardEntity board = new BoardEntity();
+    private List<CardEntity> cards = new ArrayList<>();
 }
