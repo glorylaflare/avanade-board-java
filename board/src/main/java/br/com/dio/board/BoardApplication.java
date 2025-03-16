40654944c7a1bc8c2663cd71cfd3587860f0e77d
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-15 21:43:52 -0300

    setting up the main file

--- board/src/main/java/br/com/dio/board/BoardApplication.java
+++ board/src/main/java/br/com/dio/board/BoardApplication.java
@@ -1,13 +1,21 @@
 package br.com.dio.board;
 
-import org.springframework.boot.SpringApplication;
+import br.com.dio.board.persistence.migration.MigrationStrategy;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
+
+import static br.com.dio.board.persistence.config.ConnectionConfig.getConnection;
 
 @SpringBootApplication
 public class BoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
+		try(var connection = getConnection()){
+			new MigrationStrategy(connection).executeMigration();
+		} catch (SQLException e) {
+            e.printStackTrace();
+        }
 	}
 
 }
