3439b947368cba4cfdca01ce09844a04a437b918
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-15 23:46:47 -0300

    adding methods to BoardEntity

--- board/src/main/java/br/com/dio/board/persistence/entity/BoardEntity.java
+++ board/src/main/java/br/com/dio/board/persistence/entity/BoardEntity.java
@@ -2,10 +2,26 @@ package br.com.dio.board.persistence.entity;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 @Data
 public class BoardEntity {
 
     private Long id;
     private String name;
+    private List<BoardColumnEntity> boardColumns = new ArrayList<>();
 
+    public BoardColumnEntity getInitialColumn() {
+        return getFilteredColumn(bc -> bc.getType().equals(BoardColumnTypeEnum.INITIAL));
+    }
+
+    public BoardColumnEntity getCancelColumn() {
+        return getFilteredColumn(bc -> bc.getType().equals(BoardColumnTypeEnum.CANCEL));
+    }
+
+    public BoardColumnEntity getFilteredColumn(Predicate<BoardColumnEntity> filter) {
+        return boardColumns.stream().filter(filter).findFirst().orElseThrow();
+    }
 }
