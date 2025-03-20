748020a77ad52a117959c37353e8984e2d96dca3
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-20 16:13:07 -0300

    att the variables names to snake_case

--- board/src/main/java/br/com/dio/board/persistence/dao/BlockDAO.java
+++ board/src/main/java/br/com/dio/board/persistence/dao/BlockDAO.java
@@ -15,7 +15,7 @@ public class BlockDAO {
     private final Connection connection;
 
     public void block(final String reason, final Long cardId) throws SQLException {
-        var sql = "INSERT INTO BLOCKS (blockDate, blockReason, card_id) " +
+        var sql = "INSERT INTO BLOCKS (block_date, block_reason, card_id) " +
                 "VALUES (?, ?, ?);";
 
         try(PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -28,9 +28,9 @@ public class BlockDAO {
     }
 
     public void unblock(final String reason, final Long cardId) throws SQLException {
-        var sql = "UPDATE BLOCKS SET unblockDate = ?, unblockReason = ? " +
+        var sql = "UPDATE BLOCKS SET unblock_date = ?, unblock_reason = ? " +
                 "WHERE card_id = ? " +
-                "AND unblockReason IS NULL;";
+                "AND unblock_reason IS NULL;";
 
         try(PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setTimestamp(1, toTimestamp(OffsetDateTime.now()));
