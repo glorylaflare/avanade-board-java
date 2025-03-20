5d6c77973eb836d18e117b2635a4df8bb1218308
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-20 01:44:40 -0300

    refactor just changing indentation

--- board/src/main/java/br/com/dio/board/persistence/entity/BoardColumnTypeEnum.java
+++ board/src/main/java/br/com/dio/board/persistence/entity/BoardColumnTypeEnum.java
@@ -6,8 +6,9 @@ public enum BoardColumnTypeEnum {
 
     INITIAL, FINAL, CANCEL, PENDING;
 
-    public static BoardColumnTypeEnum findByNome(final String name) {
-        return Stream.of(BoardColumnTypeEnum.values()).filter(b -> b.name().equals(name)).findFirst().orElseThrow();
+    public static BoardColumnTypeEnum findByName(final String name) {
+        return Stream.of(BoardColumnTypeEnum.values()).filter(b -> b.name()
+                .equals(name)).findFirst().orElseThrow();
     }
 
 }
