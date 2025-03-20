cf82357c8c3d9cb6dd459af23282493618d71548
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-20 19:25:00 -0300

    add the default current_timestamp parameter

--- board/src/main/resources/db/changelog/migrations/db.changelog-202503200210.sql
+++ board/src/main/resources/db/changelog/migrations/db.changelog-202503200210.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
 --changeset marcelo:202503200210
---comment: add date_added and date_moved
+--comment: add date_created and date_moved
 
 ALTER TABLE CARDS
-    ADD COLUMN date_added TIMESTAMP,
+    ADD COLUMN date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     ADD COLUMN date_moved TIMESTAMP;
 
---rollback ALTER TABLE CARDS DROP COLUMN date_added, DROP COLUMN date_moved;
+--rollback ALTER TABLE CARDS DROP COLUMN date_created, DROP COLUMN date_moved;
