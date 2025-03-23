2344d12d1db996169494d494cc94c7f0b8fe631a
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-22 22:48:17 -0300

    remove the old ADD COLUMN on CARDS table

--- board/src/main/resources/db/changelog/migrations/db.changelog-202503200210.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
---changeset marcelo:202503200210
---comment: add date_created and date_moved
-
-ALTER TABLE CARDS
-    ADD COLUMN date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ADD COLUMN date_moved TIMESTAMP;
-
---rollback ALTER TABLE CARDS DROP COLUMN date_created, DROP COLUMN date_moved;
