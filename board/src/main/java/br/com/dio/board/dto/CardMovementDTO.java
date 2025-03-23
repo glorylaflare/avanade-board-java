5b8f17362e656fc6e650fa5cbb374d1f87270d68
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-22 22:49:12 -0300

    add a new file CardMovementDTO

---
+++ board/src/main/java/br/com/dio/board/dto/CardMovementDTO.java
@@ -0,0 +1,12 @@
+package br.com.dio.board.dto;
+
+import java.time.OffsetDateTime;
+
+public record CardMovementDTO(
+        Long id,
+        OffsetDateTime entered_at,
+        OffsetDateTime lefted_at,
+        Long card_id,
+        Long board_column_id,
+        String column_type) {
+}
