b123f2d823bf660d396960e13425e4237ca44a24
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-22 22:48:41 -0300

    add a new migration to create a new table called CARD_MOVEMENT_HISTORY

---
+++ board/src/main/resources/db/changelog/migrations/db.changelog-202503212204.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset marcelo:202503212204
+--comment: card_movement_history table create
+
+CREATE TABLE CARD_MOVEMENT_HISTORY (
+    id BIGSERIAL PRIMARY KEY,
+    entered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    lefted_at TIMESTAMP,
+    card_id BIGINT NOT NULL,
+    board_column_id BIGINT NOT NULL,
+    CONSTRAINT cards__movements_fk FOREIGN KEY (card_id) REFERENCES cards(id),
+    CONSTRAINT boards_columns__cards_fk FOREIGN KEY (board_column_id) REFERENCES boards_columns(id)
+);
+
+--rollback DROP TABLE CARD_MOVEMENT_HISTORY
