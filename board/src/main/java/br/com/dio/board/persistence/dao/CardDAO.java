a1f9bd66062b630209054aab06151c6cc532deed
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-03-22 22:51:13 -0300

    add the new insert e update sql constant to add values into CARD_MOVEMENT_HISTORY table

--- board/src/main/java/br/com/dio/board/persistence/dao/CardDAO.java
+++ board/src/main/java/br/com/dio/board/persistence/dao/CardDAO.java
@@ -1,14 +1,18 @@
 package br.com.dio.board.persistence.dao;
 
-import br.com.dio.board.persistence.entity.CardEntity;
 import br.com.dio.board.dto.CardDetailsDTO;
+import br.com.dio.board.persistence.entity.CardEntity;
 import lombok.AllArgsConstructor;
 
-import java.sql.*;
-import java.time.LocalDateTime;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
 import java.util.Optional;
 
 import static br.com.dio.board.persistence.converter.OffsetDateTimeConverter.toOffsetDateTime;
+import static br.com.dio.board.persistence.converter.OffsetDateTimeConverter.toTimestamp;
 import static java.util.Objects.nonNull;
 
 @AllArgsConstructor
@@ -19,41 +23,81 @@ public class CardDAO {
     public CardEntity insert(final CardEntity entity) throws SQLException {
         var sql = "INSERT INTO CARDS (title, description, board_column_id) " +
                 "VALUES (?, ?, ?);";
+        var insertMovementSql = "INSERT INTO CARD_MOVEMENT_HISTORY (card_id, board_column_id, entered_at) " +
+                "VALUES (?, ?, ?);";
 
-        try(PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
-            statement.setString(1, entity.getTitle());
-            statement.setString(2, entity.getDescription());
-            statement.setLong(3, entity.getBoardColumn().getId());
-            int affectedRows = statement.executeUpdate();
+        try {
+            connection.setAutoCommit(false);
 
-            if(affectedRows == 0) {
-                throw new SQLException("Falha ao inserir no banco de dados, nenhuma linha afetada para o Card: " + entity.getId());
-            }
+            try(PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
-            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    entity.setId(generatedKeys.getLong(1));
-                    System.out.println("O Card foi criado com sucesso!");
-                } else {
-                    throw new SQLException("Falha ao obter a chave gerada para o novo Card.");
+                statement.setString(1, entity.getTitle());
+                statement.setString(2, entity.getDescription());
+                statement.setLong(3, entity.getBoardColumn().getId());
+                statement.executeUpdate();
+
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        entity.setId(generatedKeys.getLong(1));
+
+                        try(PreparedStatement insertMovementStatement = connection.prepareStatement(insertMovementSql)) {
+                            insertMovementStatement.setLong(1, entity.getId());
+                            insertMovementStatement.setLong(2, entity.getBoardColumn().getId());
+                            insertMovementStatement.setTimestamp(3, toTimestamp(OffsetDateTime.now()));
+                            insertMovementStatement.executeUpdate();
+                        }
+                    } else {
+                        throw new SQLException("Falha ao obter a chave gerada para o novo Card.");
+                    }
                 }
             }
+            connection.commit();
+            System.out.println("O Card foi criado com sucesso!");
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new SQLException("Erro ao criar o card.", e);
+        } finally {
+            connection.setAutoCommit(false);
         }
         return entity;
     }
 
     public void moveToColum(final Long columnId, final Long cardId) throws SQLException {
-        var sql = "UPDATE CARDS SET board_column_id = ?, date_moved = ? " +
+        var updateCardSql = "UPDATE CARDS SET board_column_id = ? " +
                 "WHERE id = ?;";
+        var updateMovementSql = "UPDATE CARd_MOVEMENT_HISTORY SET lefted_at = ? " +
+                "WHERE card_id = ? " +
+                "AND lefted_at IS NULL";
+        var insertMovementSql = "INSERT INTO CARD_MOVEMENT_HISTORY (card_id, board_column_id, entered_at) " +
+                "VALUES (?, ?, ?);";
 
-        try(PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setLong(1, columnId);
-            statement.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
-            statement.setLong(3, cardId);
-            statement.executeUpdate();
-            System.out.println("O CardBoard de id " + cardId + " foi movido com sucesso!");
+        try(PreparedStatement updateCardStatement = connection.prepareStatement(updateCardSql);
+            PreparedStatement updateMovementStatement = connection.prepareStatement(updateMovementSql);
+            PreparedStatement insertMovementStatement = connection.prepareStatement(insertMovementSql)) {
+
+            connection.setAutoCommit(false);
+
+            updateCardStatement.setLong(1, columnId);
+            updateCardStatement.setLong(2, cardId);
+            updateCardStatement.executeUpdate();
+
+            updateMovementStatement.setTimestamp(1, toTimestamp(OffsetDateTime.now()));
+            updateMovementStatement.setLong(2, cardId);
+            updateMovementStatement.executeUpdate();
+
+            insertMovementStatement.setLong(1, cardId);
+            insertMovementStatement.setLong(2, columnId);
+            insertMovementStatement.setTimestamp(3, toTimestamp(OffsetDateTime.now()));
+            insertMovementStatement.executeUpdate();
+
+            connection.commit();
+
+            System.out.println("O card de id [" + cardId + "] foi movido e sua movimentação foi registrada com sucesso!");
         } catch (SQLException e) {
+            connection.rollback();
             throw new SQLException("Erro ao mover o cartão para a coluna.", e);
+        } finally {
+            connection.setAutoCommit(false);
         }
     }
 
